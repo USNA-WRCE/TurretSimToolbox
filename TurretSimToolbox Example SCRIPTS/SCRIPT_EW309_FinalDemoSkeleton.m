8bbfe214bf31047ec7b79dc97429e5580aff89ca
Michael Kutzer <kutzer@usna.edu>
2023-08-21 14:39:36 -0400

    Update SCRIPT_EW309_FinalDemoSkeleton.m

--- TurretSimToolbox Example SCRIPTS/SCRIPT_EW309_FinalDemoSkeleton.m
+++ TurretSimToolbox Example SCRIPTS/SCRIPT_EW309_FinalDemoSkeleton.m
@@ -14,13 +14,16 @@ axs = axes('Parent',fig);
 
 %% (1.1) Define Test Parameters
 % -> Value(s) that need to be defined by the user:
-% (a) Target
-targetRange    = 170;           % STUDENT DEFINED VALUE IN CENTIMETERS
-targetDiameter = 12.7;          % STUDENT DEFINED VALUE IN CENTIMETERS
-% (b) Probability
-pOne           = 0.95;          % STUDENT DEFINED VALUE BETWEEN 0 AND 1
-% (c) Control
 
+% (a) Target
+targetRange    = 170;           % INSTRUCTOR DEFINED VALUE IN CENTIMETERS
+targetDiameter = 12.7;          % INSTRUCTOR DEFINED VALUE IN CENTIMETERS
+
+% (b) Probability
+pOne           = 0.95;          % INSTRUCTOR DEFINED VALUE BETWEEN 0 AND 1
+
+% (c) Control - Student defined controller gains and final time for
+%               turrent dynamics simulation. 
 Kp  = 1.0;  % STUDENT DEFINED VALUE, Proportional Gain (Kp)
 Ki  = 2.0;  % STUDENT DEFINED VALUE, Integral Gain (Ki)
 Kd  = 3.0;  % STUDENT DEFINED VALUE, Derivative Gain (Kd)
@@ -31,6 +34,9 @@ tf  = 5;    % STUDENT DEFINED VALUE IN SECONDS, Turret Controller Stop Time
 nShots = 10;    % Number of Shots to Fire
 xBias = -2.0;   % Target X-bias (cm)
 yBias = 1.5;    % Target Y-bias (cm)
+
+% EXAMPLE FUNCTION SYNTAX:
+% This is an example of a student-designed function
 % [xBias,yBias,nShots] = NerfGunStats(targetRange,targetDiameter/2,pOne);
 
 %% (1.3) Create Simulated Target
@@ -51,17 +57,27 @@ drawnow
 % -> Value(s) that need to be calculated:
 % xPixels = ???
 % yPixels = ???
+
+% EXAMPLE FUNCTION SYNTAX:
+% This is an example of a student-designed function
 % [xPixels,yPixels] = STUDENTFUNCTION(im);
 
 %% (3.2) Locate Target in Centimeters
 % -> Value(s) that need to be calculated:
 % x_cm = ???
 % y_cm = ???
+
+% EXAMPLE FUNCTION SYNTAX:
+% This is an example of a student-designed function
 % [x_cm,y_cm] = STUDENTFUNCTION(targetRange,xPixels,yPixels);
 
 %% (4) Calculate Desired Turret Angle
 % -> Value(s) that need to be calculated:
 thetaDesired = deg2rad(-10);    % Desired Angle (radians)
+
+% EXAMPLE FUNCTION SYNTAX:
+% This is an example of a student-designed function
+% thetaDesired = STUDENTFUNCTION(x_cm,xBias,targetRange);
 
 %% (5.1) Package Control Parameters
 cParams.Kp = Kp;
